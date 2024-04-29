0fe9e7d560569cb7c4bae29b881da3864270b65a
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-04-29 12:17:46 +0900

    240429 ydg

---
+++ src/main/java/com/james/api/team/model/TeamDto.java
@@ -0,0 +1,29 @@
+package com.james.api.team.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Log4j2
+public class TeamDto {
+    private Long id;
+    private String regionName;
+    private String teamName;
+    private String eTeamName;
+    private String originYear;
+    private Long stadiumId;
+    private String zipCode1;
+    private String zipCode2;
+    private String address;
+    private String ddd;
+    private String tel;
+    private String fax;
+    private String homepage;
+    private String owner;
+
+}
